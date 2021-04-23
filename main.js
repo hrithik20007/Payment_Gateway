e7ed084b7e7d739810aa2abc800495004ddfb6ab
hrithik20007 <darthvader20137@gmail.com>
2021-04-24 03:34:48 +0530

    Added main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+//window.open(pathString, target);
+function DonateTab() {
+	window.open("./donate.html", "_blank");
+};
+
+//For alert message and resetting the given values
+//var myform = document.getElementById('contact');
+//myform.addEventListener('submit',onSubmit);
+
+//function onSubmit() {
+//	alert("Form has been submitted successfully!");
+//};
