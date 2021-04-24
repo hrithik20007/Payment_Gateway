21ec720dc2b8f962e25051ff4d3030d10ea4acd4
hrithik20007 <darthvader20137@gmail.com>
2021-04-25 02:42:42 +0530

    More Responsive!

--- main.js
+++ main.js
@@ -3,6 +3,30 @@ function DonateTab() {
 	window.open("./donate.html", "_blank");
 };
 
+function openSlide1() {
+	document.getElementById("side-menu1").style.width="250px";
+};
+
+function closeSlide1() {
+	document.getElementById("side-menu1").style.width="0";
+};
+
+function openSlide2() {
+	document.getElementById("side-menu2").style.width="250px";
+};
+
+function closeSlide2() {
+	document.getElementById("side-menu2").style.width="0";
+};
+
+function openSlide3() {
+	document.getElementById("side-menu3").style.width="250px";
+};
+
+function closeSlide3() {
+	document.getElementById("side-menu3").style.width="0";
+};
+
 //For alert message and resetting the given values
 //var myform = document.getElementById('contact');
 //myform.addEventListener('submit',onSubmit);
