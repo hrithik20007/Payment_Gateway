3cb29561e592dd77d852b847419e97bfa175a516
hrithik20007 <darthvader20137@gmail.com>
2021-04-26 03:41:19 +0530

    Toggle Switch Added

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function DonateTab() {
 	window.open("./donate.html", "_blank");
 };
 
+
+//For opening and closing of side menu-nav bar in the different pages
 function openSlide1() {
 	document.getElementById("side-menu1").style.width="250px";
 };
@@ -27,10 +29,38 @@ function closeSlide3() {
 	document.getElementById("side-menu3").style.width="0";
 };
 
-//For alert message and resetting the given values
-//var myform = document.getElementById('contact');
-//myform.addEventListener('submit',onSubmit);
+/*
+const toggleSwitch = document.querySelector('#theme input[type="checkbox"]');
 
-//function onSubmit() {
-//	alert("Form has been submitted successfully!");
-//};
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.aboutbody.setAttribute('class', 'darktext');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+const check=document.getElementById('checkbox');
+check.addEventListener('change', ()=>{
+	document.classList.toggle('darkbg');
+});
+
+function check() {
+	document.getElementById('aboutbody').classList.toggle('darkbg');
+};
+*/
+//For alert message and resetting the given values
+/*const name=document.getElementById("namein");
+const mail=document.getElementById("mailin");
+const text=document.getElementById("msgin");
+function onSubmit() {
+	if (name.value=""||mail.value=""||text.value=""){
+		alert("Inputs Missing!");
+	}else{
+		alert("Form has been submitted successfully!");
+	}
+	name.value="";
+	mail.value="";
+	text.value="";
+};
+*/
