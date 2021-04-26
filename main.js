31d0d45ebd7ede24142e791627cc6ec969f01aaf
hrithik20007 <darthvader20137@gmail.com>
2021-04-27 00:46:22 +0530

    Contact Form Features Added

--- main.js
+++ main.js
@@ -36,6 +36,27 @@ function closeSlide3() {
 
 
 
+//For alert message and resetting the given values
+if (typeof window === 'object'){
+const name=document.getElementById("namein");
+const mail=document.getElementById("mailin");
+const text=document.getElementById("msgin");
+function onSubmit() {
+	if (name.value==""||mail.value==""||text.value==""){
+		alert("Inputs Missing!");
+	}else{
+		alert("Form has been submitted successfully!");
+	}
+	name.value="";
+	mail.value="";
+	text.value="";
+};
+}
+
+
+
+
+
 //For toggling between light and dark mode
 if (typeof window === 'object'){
 var checkbox = document.getElementById("checkbox");
@@ -45,65 +66,3 @@ checkbox.addEventListener('change',function(){
 	document.getElementById('abouthead').classList.toggle('themehead');
 });
 }
-
-
-
-
-
-/*
-if (typeof window==='object'){
-const form = document.getElementById("contactform");
-
-form.addEventListener('submit', (e)=>{
-	e.preventDefault();
-	console.log("Form Submitted!");
-});
-}
-*/
-/*
-const toggleSwitch = document.querySelector('#theme input[type="checkbox"]');
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.aboutbody.setAttribute('class', 'darktext');
-    }
-}
-
-toggleSwitch.addEventListener('change', switchTheme, false);
-
-const check=document.getElementById('checkbox');
-check.addEventListener('change', ()=>{
-	document.classList.toggle('darkbg');
-});
-
-function check() {
-	document.getElementById('aboutbody').classList.toggle('darkbg');
-};
-*/
-
-/*
-//For alert message and resetting the given values
-if (typeof window === 'object'){
-const name=document.getElementById("namein");
-const mail=document.getElementById("mailin");
-const text=document.getElementById("msgin");
-
-const emailIsValid = mail => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(mail);
-}
-
-function onSubmit() {
-	if (name.value==null||mail.value==null||text.value==null){
-		alert("Inputs Missing!");
-	}
-	if(!emailIsValid(mail.value)){
-		alert("Your Email Format is wrong!");
-	}else{
-		alert("Form has been submitted successfully!");
-	}
-	name.value="";
-	mail.value="";
-	text.value="";
-};
-}
-*/
